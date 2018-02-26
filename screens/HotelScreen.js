97f1c5f85e190556a3c3b7f841b9e555dd45eb45
Kashish Grover <kashishgrover95@gmail.com>
2018-02-27 03:00:13 +0530

    dump all data

--- screens/HotelScreen.js
+++ screens/HotelScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, ScrollView, View, Image, StyleSheet } from 'react-native';
+import { Text, ScrollView, View, Image, StyleSheet, ActivityIndicator } from 'react-native';
 import { Toast } from 'native-base';
 import { inject, observer } from 'mobx-react';
 
@@ -7,6 +7,16 @@ import Layout from '../constants/Layout';
 import Header from '../components/Header';
 
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+//This is just a demo -- The actual dates would be passed as props :)
+let today = new Date();
+let dd1 = today.getDate();
+let MM1 = months[today.getMonth()];
+
+let tomorrow = new Date(today);
+tomorrow.setDate(today.getDate() + 1);
+let dd2 = tomorrow.getDate();
+let MM2 = months[tomorrow.getMonth()];
 
 @inject('hotelStore')
 @observer
@@ -44,28 +54,62 @@ export default class HotelScreen extends React.Component {
   render() {
     const { name, locality, image, price } = this.props.hotelStore.hotels[this.id];
 
-    //This is just a demo -- The actual dates would be passed as props :)
-    let today = new Date();
-    let dd1 = today.getDate();
-    let MM1 = months[today.getMonth()];
+    const Prices = (
+      <View style={styles.textWrapper}>
+        <Text style={styles.h1}>Prices</Text>
+        {Object.keys(price).map((keyName, keyIndex) => (
+          <Text key={keyIndex} style={!price[keyName] && { textDecorationLine: 'line-through' }}>
+            <Text style={styles.sh1}>
+              {keyName.charAt(0).toUpperCase() + keyName.slice(1) + ': '}
+            </Text>
+            <Text style={styles.sh2}>{price[keyName] ? '₹ ' + price[keyName] : 'Sold Out'}</Text>
+          </Text>
+        ))}
+      </View>
+    );
 
-    let tomorrow = new Date(today);
-    tomorrow.setDate(today.getDate() + 1);
-    let dd2 = tomorrow.getDate();
-    let MM2 = months[tomorrow.getMonth()];
+    let Essentials = <View />;
+    let Policies = <View />;
+    if (!this.state.loading) {
+      Essentials = (
+        <View style={styles.textWrapper}>
+          <Text style={styles.h1}>Essentials</Text>
+          {this.details.essentials.map((val, index) => (
+            <Text key={index} style={styles.sh1}>
+              {val}
+            </Text>
+          ))}
+        </View>
+      );
+      Policies = (
+        <View style={styles.textWrapper}>
+          <Text style={styles.h1}>Policies</Text>
+          {this.details.policies.map((val, index) => (
+            <Text key={index} style={styles.sh1}>
+              {val}
+            </Text>
+          ))}
+        </View>
+      );
+    }
 
     return (
-      <ScrollView style={styles.container}>
+      <View>
         <Header
           navigation={this.props.navigation}
           h1={`Treebo ${name}, ${locality}`}
           h2={`${dd1} ${MM1} - ${dd2} ${MM2}`}
         />
-        <View style={styles.imageContainer}>
-          <Image source={{ uri: image }} style={styles.image} />
-        </View>
-        <Text>{JSON.stringify(price)}</Text>
-      </ScrollView>
+        <ScrollView style={styles.container}>
+          <View style={styles.imageContainer}>
+            <Image source={{ uri: image }} style={styles.image} />
+          </View>
+          {Prices}
+          {!this.state.loading ? Essentials : <ActivityIndicator size="large" />}
+          {!this.state.loading && Policies}
+          <View style={styles.bottomPadder} />
+        </ScrollView>
+      </View>
     );
   }
 }
@@ -87,5 +131,26 @@ const styles = StyleSheet.create({
     height: 268,
     margin: 16,
     borderRadius: 4,
+  },
+  textWrapper: {
+    paddingLeft: 16,
+    marginBottom: 20,
+  },
+  h1: {
+    fontWeight: '500',
+    fontSize: 20,
+    color: '#0eb550',
+  },
+  sh1: {
+    fontWeight: '500',
+    fontSize: 16,
+    color: '#212121',
+  },
+  sh2: {
+    fontSize: 16,
+    color: '#888',
+  },
+  bottomPadder: {
+    height: 80,
   },
 });
